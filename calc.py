9baea0e28478eb0794a0ee975205d0c00d31ecde
Lungu Constantin-Marian <111748828+CMarian02@users.noreply.github.com>
2022-11-24 00:27:02 +0200

    floating point updated

--- calc.py
+++ calc.py
@@ -1,5 +1,6 @@
 # Main App
 import tkinter as tk
+from decimal import *
 from screeninfo import get_monitors
 
 class MainApp(tk.Tk):
@@ -44,84 +45,165 @@ class MainPage(tk.Frame):
         button_frame.pack(fill = 'both', expand = True)
         NonePhoto = tk.PhotoImage(width = 1, height = 1)
         #color #1B262C
-        button_clearall = tk.Button(button_frame, text = "AC", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_clearall = tk.Button(button_frame, text = "AC", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("AC", 0))
         button_clearall.grid(row = 0, column = 0, sticky = tk.W)
-        button_clearone = tk.Button(button_frame, text = "CC", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_clearone = tk.Button(button_frame, text = "CC", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("CC", 0))
         button_clearone.grid(row = 0, column = 1, stick = tk.W)
-        button_mod = tk.Button(button_frame, text = "%", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_mod = tk.Button(button_frame, text = "%", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("%", 0))
         button_mod.grid(row = 0, column = 2, sticky = tk.W)
-        button_div = tk.Button(button_frame, text = "/", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =100, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_div = tk.Button(button_frame, text = "/", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =100, height = 80, cursor = "hand2", command = lambda:self.button_pressed("/", 0))
         button_div.grid(row = 0, column = 3, sticky = tk.W)
-        button_sev = tk.Button(button_frame, text = "7", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_sev = tk.Button(button_frame, text = "7", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("Number", 7))
         button_sev.image = NonePhoto
         button_sev.grid(row = 1, column = 0)
-        button_eig = tk.Button(button_frame, text = "8", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_eig = tk.Button(button_frame, text = "8", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("Number", 8))
         button_eig.grid(row = 1, column = 1)
-        button_nin = tk.Button(button_frame, text = "9", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_nin = tk.Button(button_frame, text = "9", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("Number", 9))
         button_nin.grid(row = 1, column = 2)
-        button_mul = tk.Button(button_frame, text = "X", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =100, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_mul = tk.Button(button_frame, text = "X", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =100, height = 80, cursor = "hand2", command = lambda:self.button_pressed("X", 0))
         button_mul.grid(row = 1, column = 3)
-        button_fou = tk.Button(button_frame, text = "4", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_fou = tk.Button(button_frame, text = "4", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("Number", 4))
         button_fou.grid(row = 2, column = 0)
-        button_fiv = tk.Button(button_frame, text = "5", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_fiv = tk.Button(button_frame, text = "5", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("Number", 5))
         button_fiv.grid(row = 2, column = 1)
-        button_six = tk.Button(button_frame, text = "6", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_six = tk.Button(button_frame, text = "6", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("Number", 6))
         button_six.grid(row = 2, column = 2)
-        button_min = tk.Button(button_frame, text = "-", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =100, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_min = tk.Button(button_frame, text = "-", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =100, height = 80, cursor = "hand2", command = lambda:self.button_pressed("-", -1))
         button_min.grid(row = 2, column = 3)
         button_one = tk.Button(button_frame, text = "1", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("Number", 1))
         button_one.grid(row = 3, column = 0)
-        button_two = tk.Button(button_frame, text = "2", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("Number",2))
+        button_two = tk.Button(button_frame, text = "2", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("Number", 2))
         button_two.grid(row = 3, column = 1)
-        button_thr = tk.Button(button_frame, text = "3", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_thr = tk.Button(button_frame, text = "3", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("Number", 3))
         button_thr.grid(row = 3, column = 2)
         button_plu = tk.Button(button_frame, text = "+", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =100, height = 80, cursor = "hand2", command = lambda:self.button_pressed("+", 0))
         button_plu.grid(row = 3, column = 3)
-        button_next_pag = tk.Button(button_frame, text = ">>", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_next_pag = tk.Button(button_frame, text = ">>", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed(">>", 0))
         button_next_pag.grid(row = 4, column = 0)
-        button_zer = tk.Button(button_frame, text = "0", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_zer = tk.Button(button_frame, text = "0", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed("Number", 0))
         button_zer.grid(row = 4, column = 1)
-        button_dig = tk.Button(button_frame, text = ",", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_dig = tk.Button(button_frame, text = ",", font = ('Bebas Neue', 20), image = NonePhoto, compound = "c", width =140, height = 80, cursor = "hand2", command = lambda:self.button_pressed(",", 0))
         button_dig.grid(row = 4, column = 2)
-        button_ans = tk.Button(button_frame, text = "=", font = ('bebas Neue', 20), image = NonePhoto, compound = "c", width =100, height = 80, cursor = "hand2", command = lambda:self.check_btn())
+        button_ans = tk.Button(button_frame, text = "=", font = ('bebas Neue', 20), image = NonePhoto, compound = "c", width =100, height = 80, cursor = "hand2", command = lambda:self.button_pressed("=", 0))
         button_ans.grid(row = 4, column = 3)
+        
         global times
         global number_one
         global number_two
         global operator
         global nr
+        global z
+        global n
+        z = 1
+        n = 0
         nr = 0
         times = 0
-    def check_btn(self):
-        print('Checked')
-
+    
     def button_pressed(self, button_type, number):
         global number_one
         global number_two
         global operator
         global times
         global nr
-        
-        print('test confirmed')
-        while button_type == "Number":
+        global z
+        global n
+
+        if button_type == "Number" and nr == 0:
             if times == 0:
                 number_one = number
                 times += 1
-            number_one = number_one * 10 + number
-            button_type = None
-            print(f'number one is: {number_one}')
+            elif times == -1:
+                z *= 10
+                if z <= pow(10,15):
+                    n += 1
+                    number_one = (number_one * z + number)/z
+                    number_one = round(number_one, n)
+                    print(f"Number one: {number_one}")
+                else:
+                    print("You try to enter many numbers!")
+            else:
+                number_one = number_one * 10 + number
+                
         if button_type == "+" and times != 0:
-            operator = 1
+            operator = "+"
             nr = 2
             times = 0
-        while button_type == "Number" and nr == 2:
+            z = 1
+            n = 1
+        if button_type == "-" and times != 0:
+            operator = "-"
+            nr = 2
+            times = 0
+            n = 1
+        if button_type == "X" and times != 0:
+            operator = "X"
+            nr = 2
+            times = 0
+            n = 1
+        if button_type == "/" and times != 0:
+            operator = "/"
+            nr = 2
+            times = 0 
+            n = 1
+        if button_type == "%" and times != 0:
+            operator = "%"
+            nr = 2
+            times = 0
+            n = 1
+        if button_type == "," and times != 0:
+            times = -1
+            operator = ","
+        if button_type == "Number" and nr == 2:
             if times == 0:
                 number_two = number
                 times += 1
-            number_two = number_two * 10 + number
-            button_type = None
-            print(f"number two is:{number_two}")
-        
+            elif times == -1:
+                z *= 10
+                if z <= pow(10,1):
+                    n +=1
+                    number_two = (number_two * z + number)/z
+                    number_two = round(number_two, n)
+                    print(f"Number two: {number_two}")
+                else:
+                    print("You try to enter many numbers!")
+            else:
+                number_two = number_two * 10 + number
+        if button_type == "AC":
+            operator = "AC"
+        if button_type == "CC":
+            if nr != 2:
+                operator = "CC1"
+            else:
+                operator = "CC2"
+
+
+        if button_type == "=" and times != 0:
+            if operator == "+":
+                display = number_one + number_two
+            if operator == "-":
+                display = number_one - number_two
+            if operator == "X":
+                display = number_one * number_two
+            if operator == "/":
+                display = number_one/number_two
+            if operator == "%":
+                display = number_one%number_two
+            if operator == "AC":
+                pass
+            if operator == "CC":
+                pass
+            times = 0
+            number_one = 0
+            number_two = 0
+            nr = 0
+            z = 1 
+            print(display)
+
+
+
+
+
+
 if __name__ == "__main__":
     app = MainApp()
     app.mainloop()
